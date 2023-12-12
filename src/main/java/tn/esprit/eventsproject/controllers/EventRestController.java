b50dbf201853af42e64347e880bd5c599d7119f0
MohamedOurag <93941990+MohamedOuarg@users.noreply.github.com>
2023-12-12 21:20:12 +0100

    Update EventRestController.java

--- src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
+++ src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
@@ -46,6 +46,7 @@ public class EventRestController {
     //        return eventServices.addAffectLog(logistics,descriptionEvent);
      //        return eventServices.addAffectLog(logistics,descriptionEvent);
     //        return eventServices.addAffectLog(logistics,descriptionEvent);
+    //        return eventServices.addAffectLog(logistics,descriptionEvent);
 
 
 
