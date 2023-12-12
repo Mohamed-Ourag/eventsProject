9cbb9c2133bb255db0daee6823f39e954016c1a4
MohamedOurag <93941990+MohamedOuarg@users.noreply.github.com>
2023-12-12 13:17:44 +0100

    Update EventRestControllerrr.java

--- src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
+++ src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
@@ -36,6 +36,8 @@ public class EventRestController {
 //    public Logistics addAffectLog(@RequestBody Logistics logistics,@PathVariable("description") String descriptionEvent){
 //        return eventServices.addAffectLog(logistics,descriptionEvent);
 //    }
+
+      //    @PutMapping("/addAffectLog/{description}")
     @GetMapping("/getLogs/{d1}/{d2}")
     public List<Logistics> getLogistiquesDates (@PathVariable("d1") LocalDate date_debut, @PathVariable("d2") LocalDate date_fin){
         return eventServices.getLogisticsDates(date_debut,date_fin);
